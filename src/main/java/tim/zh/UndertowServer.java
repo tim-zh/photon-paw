3cfa184c857d036c9c677364b482a3b6e69922b4
tim-zh <tim.zhlobich@gmail.com>
2017-12-07 00:38:59 +0300

    default js client

--- src/main/java/tim/zh/UndertowServer.java
+++ src/main/java/tim/zh/UndertowServer.java
@@ -2,6 +2,7 @@ package tim.zh;
 
 import io.undertow.Undertow;
 import io.undertow.server.handlers.resource.FileResourceManager;
+import io.undertow.util.Headers;
 import io.undertow.websockets.core.AbstractReceiveListener;
 import io.undertow.websockets.core.BufferedTextMessage;
 import io.undertow.websockets.core.WebSocketChannel;
@@ -9,6 +10,8 @@ import io.undertow.websockets.core.WebSockets;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 import static io.undertow.Handlers.path;
@@ -22,7 +25,15 @@ public class UndertowServer implements UiServer {
   @Override
   public void start(String host, int port, int wsPort, String resourceRoot, Consumer<String> wsCallback) {
     server = Undertow.builder()
-        .addHttpListener(port, host, path().addPrefixPath("/", resource(new FileResourceManager(new File(resourceRoot)))))
+        .addHttpListener(port, host, path()
+            .addPrefixPath("/givemeui_client.js", exchange -> {
+              exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/javascript");
+              String response = readFile("givemeui_client.js")
+                  .replace("HOST", host)
+                  .replace("PORT", wsPort + "");
+              exchange.getResponseSender().send(response);
+            })
+            .addPrefixPath("/", resource(new FileResourceManager(new File(resourceRoot)))))
         .addHttpListener(wsPort, host, path().addPrefixPath("/", websocket((exchange, channel) -> {
           ws = channel;
           ws.getReceiveSetter().set(new MyAbstractReceiveListener(wsCallback));
@@ -30,6 +41,12 @@ public class UndertowServer implements UiServer {
         })))
         .build();
     server.start();
+  }
+
+  private String readFile(String path) {
+    InputStream stream = UndertowServer.class.getClassLoader().getResourceAsStream(path);
+    Scanner s = new Scanner(stream).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
   }
 
   @Override
